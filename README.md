3c31c20102ed0f9e525d39ba5dca7f373c168d68
slarsen1706 <45298641+slarsen1706@users.noreply.github.com>
2026-09-07 11:42:56 +0200

    Initial commit
    All files added

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# CV - Sander Larsen
+
+## Beskrivelse:
+
+CV-en ble laget som en øvelse i grunnleggende webutvikling.  
+Følgende programmeringsspråk ble brukt:
+
+- HTML
+- CSS
+- JavaScript
+
+Prosjektet ble utviklet i ***JetBrains - WebStorm***. IDE'en er  
+gratis for hobbyutvikling og utdanning, men betalt gjennom ett  
+abonnement for næringsdrivende
---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("homeBtn").addEventListener('click', function(){
+    window.location.href = "index.html?_ijt=a6hk9jardetk6dirpqmmebmhnn&_ij_reload=RELOAD_ON_SAVE"
+    console.log("Home page");
+})
+
+document.getElementById("aboutBtn").addEventListener('click', function(){
+    window.location.href = "about.html?_ijt=a6hk9jardetk6dirpqmmebmhnn&_ij_reload=RELOAD_ON_SAVE"
+    console.log("About page");
+})
+
+document.getElementById("projectsBtn").addEventListener('click', function(){
+    window.location.href = "projects.html?_ijt=a6hk9jardetk6dirpqmmebmhnn&_ij_reload=RELOAD_ON_SAVE"
+    console.log("Project page");
+})
